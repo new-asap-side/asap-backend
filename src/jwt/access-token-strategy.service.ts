90c82806e833060e5f1d779a1eb825a7c598efd5
TaeSungLee <ts.lee1996y@gmail.com>
2025-02-09 14:25:39 +0900

    [HOTFIX]이태성/엑세스 토큰 검증쪽 로깅 추가_2

--- src/jwt/access-token-strategy.service.ts
+++ src/jwt/access-token-strategy.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
 
 @Injectable()
 export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt-access') {
@@ -10,14 +11,19 @@ export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt-access'
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get<string>('JWT_SECRET_KEY'),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_SECRET_KEY')
+      passReqToCallback: true,
     });
   }
 
-  async validate(payload: any) {
+  async validate(req: Request, payload: any) {
+    const accessToken = req.get('authorization').split('Bearer ')[1];
     console.log(`
     [Access token validate]
+    [Method]: ${req.method}, 
+    [Path]: ${req.route.path}, 
+    [Token]: ${accessToken},
     [Payload]: ${JSON.stringify(payload)}
     `);
     return payload;
